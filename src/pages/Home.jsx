534cb04b01bd0ca048ee14276f84e381de61dca5
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2023-01-30 12:03:59 +0100

    added login page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Box, Button } from "@mui/material"
+
+export default function Home() {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                gap: 5,
+            }}
+        >
+            <Button size="large" variant="contained">
+                Hej Spotify
+            </Button>
+        </Box>
+    )
+}
